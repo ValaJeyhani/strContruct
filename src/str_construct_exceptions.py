c1adef46856bd3289df45b2a774449d45213eb8b
Vala Jeyhani <vala.jeyhani@yahoo.com>
2023-10-08 10:02:44 -0400

    Add StrInt
    A simple class (and a base class which will be required by all Str
    Construct objects) to handle build and parse. Currently, the parsing
    uses the Parse package (https://pypi.org/project/parse/). But it
    appears to have some problems when compared to the formats supported
    by the `format` method. The corresponding test cases are marked
    with xfail.
    
    Also, added the minimal set of files to run unit tests by pytest.

---
+++ src/str_construct_exceptions.py
@@ -0,0 +1,14 @@
+
+class StrConstructError(ValueError):
+    """_summary_
+
+    Args:
+        ValueError (_type_): _description_
+    """
+
+class StrConstructMismatchingFormatError(StrConstructError):
+    """_summary_
+
+    Args:
+        StrConstructError (_type_): _description_
+    """
