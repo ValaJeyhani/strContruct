92b402cbef709067088b0edab3e98519b7b8bd5c
Vala Jeyhani <vala.jeyhani@yahoo.com>
2023-10-11 19:12:14 -0400

    Fixed a but in StrInt for multi-digit lengths (#10)

--- test/unit/test_str_int.py
+++ test/unit/test_str_int.py
@@ -33,9 +33,12 @@ class TestStrInt:
 
     def test_build_hex_padding(self):
         assert StrInt("03X").build(10) == "00A"
+        assert StrInt("013X").build(10) == "000000000000A"
 
     def test_parse_hex_padding(self):
         assert StrInt("03X").parse("00A") == 10
+        assert StrInt("013X").parse("000000000000A") == 10
+        assert StrInt("13X").parse("000000000000A") == 10
 
     def test_parse_longer_input_than_format(self):
         assert StrInt("03X").parse("00A1394756") == 10
@@ -47,4 +50,7 @@ class TestStrInt:
     def test_parse_shorter_input_than_format(self):
         with pytest.raises(StrConstructParseError) as info:
             StrInt("3d").parse("12")
+            assert info.value.message.startswith("Insufficient characters found")
+        with pytest.raises(StrConstructParseError) as info:
+            StrInt("13X").parse("00000000000A")
             assert info.value.message.startswith("Insufficient characters found")
