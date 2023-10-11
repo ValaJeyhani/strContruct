5a5f5ecdcc034b143fbc7fad8bd5a0477cf06eb4
Vala Jeyhani <vala.jeyhani@yahoo.com>
2023-10-10 22:32:22 -0400

    A more strict parse algorithm for StrFloat
    StrFloat now consumes only as many characters as specified by
    the format and strictly requires that many.

--- test/unit/test_str_float.py
+++ test/unit/test_str_float.py
@@ -6,7 +6,7 @@ import pytest
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../src"))
 
 from str_float import StrFloat
-from str_construct_exceptions import StrConstructMismatchingFormatError
+from str_construct_exceptions import StrConstructParseError
 
 class TestStrFloat:
     def test_build_no_decimal(self):
@@ -23,25 +23,30 @@ class TestStrFloat:
         assert StrFloat(".1f").build(2.0) == "2.0"
         assert StrFloat(".1f").build(2.0182374) == "2.0"
         assert StrFloat(".1f").build(2.0989898) == "2.1"
+        assert StrFloat(".1f").build(12345.0989898) == "12345.1"
 
     def test_parse_one_decimal(self):
         assert StrFloat(".1f").parse("2.1") == 2.1
         assert StrFloat(".1f").parse("2.0") == 2
 
-    @pytest.mark.xfail()
-    def test_parse_one_decimal_fails_with_more_decimals_input(self):
-        with pytest.raises(StrConstructMismatchingFormatError):
-            StrFloat(".1f").parse("2.0182374")
-        with pytest.raises(StrConstructMismatchingFormatError):
-            StrFloat(".1f").parse("2.0989898")
+    def test_parse_one_decimal_fails_with_less_decimals_input(self):
+        with pytest.raises(StrConstructParseError):
+            StrFloat(".4f").parse("2.018")
+        with pytest.raises(StrConstructParseError):
+            StrFloat(".10f").parse("2.111111111")
+        assert StrFloat(".10f").parse("2.1111111111") == 2.1111111111
 
     def test_build_six_decimal(self):
         assert StrFloat(".6f").build(2) == "2.000000"
         assert StrFloat(".6f").build(2.0000001) == "2.000000"
         assert StrFloat(".6f").build(2.0000009) == "2.000001"
 
-    @pytest.mark.xfail()
-    def test_parse_six_decimal(self):
-        assert StrFloat(".6f").parse("2.000000") == 2
+    def test_parse_two_digit_format(self):
+        assert StrFloat(".10f").parse("2.1234567891") == 2.1234567891
+
+    def test_parse_consumes_as_many_chars_as_specified_in_format(self):
+        assert StrFloat(".1f").parse("2.1b") == 2.1
+        assert StrFloat(".2f").parse("2.12W") == 2.12
+        assert StrFloat(".2f").parse("2.20Foo") == 2.20
         assert StrFloat(".6f").parse("2.0000001") == 2.000000
-        assert StrFloat(".6f").parse("2.0000009") == 2.000001
+        assert StrFloat(".6f").parse("2.0000009") == 2.000000
